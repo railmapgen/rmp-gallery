1bde832b5dbf9bf0065e997285d394c129e5084f
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2023-05-01 23:24:13 +0800

    #12 Update name from seed-project to rmp-gallery

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import legacy from '@vitejs/plugin-legacy';
 
 // https://vitejs.dev/config
 export default defineConfig({
-    base: '/seed-project/',
+    base: '/rmp-gallery/',
     plugins: [
         react(),
         legacy({

6bb202d36de6185771b181e7a6fbcb20205d2419
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2023-08-31 20:36:25 +0800

    #620 Only trigger issue bot on Resources:

--- src/util/constant.ts
+++ src/util/constant.ts
@@ -6,7 +6,7 @@ export enum Events {
 }
 
 export const GITHUB_ISSUE_HEADER = 'Hi RMP team! I would like to contribute to the gallery with the data below.';
-export const GITHUB_ISSUE_PREAMBLE = '**Do not edit lines below, they are meant for bots only!!!**';
+export const GITHUB_ISSUE_PREAMBLE = '**Paste or Upload below. They are meant for BOTS ONLY!!!**';
 
 export interface MetadataDetail {
     name: Translation;

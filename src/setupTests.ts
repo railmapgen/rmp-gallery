6b4ee9186291f99fe0ad4ba7a1d4022ebdf0f6e5
Chito Wong <chi.to.wong@outlook.com>
2024-02-03 22:15:41 +0800

    #1182 Bump dependencies and support Gitee release

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,37 +1,24 @@
 import { RootState } from './redux';
 import createMockStore from 'redux-mock-store';
 import { getDefaultMiddleware, ThunkDispatch } from '@reduxjs/toolkit';
-import infoJson from '../info.json';
 import { TextEncoder } from 'util';
+import { vi } from 'vitest';
 
 // FIXME: any -> AnyAction?
 type DispatchExts = ThunkDispatch<RootState, void, any>;
 export const createMockRootStore = createMockStore<RootState, DispatchExts>(getDefaultMiddleware());
 
-class BroadcastChannel {
-    postMessage() {
-        // mocked
-    }
-
-    onmessage() {
-        // mocked
-    }
-}
-
-global.BroadcastChannel = BroadcastChannel as any;
-
 const originalFetch = global.fetch;
-global.fetch = (...args) => {
+global.fetch = vi.fn().mockImplementation((...args: any[]) => {
     if (args[0].toString().includes('/info.json')) {
         return Promise.resolve({
             ok: true,
             status: 200,
-            json: () => Promise.resolve(infoJson),
+            json: () => import('../info.json').then(module => module.default),
         }) as any;
     } else {
-        // @ts-expect-error any here :)
-        return originalFetch(...args);
+        return originalFetch(args[0], args[1]);
     }
-};
+});
 
 global.TextEncoder = TextEncoder;

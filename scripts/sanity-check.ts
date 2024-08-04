0f0a46150b48cd7f4f23b71d6e3c5e3e47343f1a
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2024-08-04 17:51:11 +0800

    #1647 Throw error when graph can not be initialized

--- scripts/sanity-check.ts
+++ scripts/sanity-check.ts
@@ -1,5 +1,4 @@
 import * as core from '@actions/core';
-import { SerializedGraph } from 'graphology-types';
 import pkg from 'graphology';
 // @ts-expect-error cjs compatibility
 const { MultiDirectedGraph } = pkg;
@@ -30,31 +29,40 @@ export const sanityCheck = async () => {
     const detailsEls = await readIssueBody();
     const { param, type } = parseDetailsEl(detailsEls);
 
-    if (type === 'real_world') {
-        const g = MultiDirectedGraph.from(param.graph as SerializedGraph<any, any, EdgeAttributes>);
-        g.forEachEdge(
-            (
-                edge: string,
-                attr: EdgeAttributes,
-                source: string,
-                target: string,
-                sourceAttr: any,
-                targetAttr: any,
-                undirected: boolean
-            ) => {
-                console.log(edge, attr.type, attr.style);
-                if (attr.type === 'simple' && noSimplePathStyle.includes(attr.style)) {
-                    core.setOutput(
-                        'message',
-                        "Your work contains line(s) that utilize the single-color style with a simple line path. To address this, you can modify the line path type to something other than the simple path in _Settings > Procedures > Change all objects' attributes_."
-                    );
-                    throw new Error(`No simple path for style: ${attr.style}`);
-                }
-            }
+    if (type !== 'real_world') return;
+
+    let g; // MultiDirectedGraph<any, any, EdgeAttributes>
+    try {
+        g = MultiDirectedGraph.from(param.graph);
+    } catch (e) {
+        core.setOutput(
+            'message',
+            'The work you upload contains corrupted graph, try again or contact contributor for help.'
         );
-        core.setOutput('message', 'pass');
-        console.log('Passed the sanity check!');
+        throw new Error('Can not read graph!');
     }
+    g.forEachEdge(
+        (
+            edge: string,
+            attr: EdgeAttributes,
+            source: string,
+            target: string,
+            sourceAttr: any,
+            targetAttr: any,
+            undirected: boolean
+        ) => {
+            console.log(edge, attr.type, attr.style);
+            if (attr.type === 'simple' && noSimplePathStyle.includes(attr.style)) {
+                core.setOutput(
+                    'message',
+                    "Your work contains line(s) that utilize the single-color style with a simple line path. To address this, you can modify the line path type to something other than the simple path in _Settings > Procedures > Change all objects' attributes_."
+                );
+                throw new Error(`No simple path for style: ${attr.style}`);
+            }
+        }
+    );
+    core.setOutput('message', 'pass');
+    console.log('Passed the sanity check!');
 };
 
 await sanityCheck();
